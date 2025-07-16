460d455e2c5d31272f4ebd28234400025157ff94
sneha0099 <snehaprajapati678@gmail.com>
2025-07-16 10:09:07 +0530

    test: add test for adding sweet

---
+++ server/src/tests/sweet.test.ts
@@ -0,0 +1,29 @@
+import request from "supertest";
+import app from "../app";
+import dotenv from "dotenv";    
+import mongoose from "mongoose";
+
+dotenv.config();
+beforeAll(async () => {
+    await mongoose.connect(process.env.MONGO_TEST_URI as string);
+});
+
+afterAll(async () => {
+    await mongoose.connection.dropDatabase();
+    await mongoose.connection.close();
+});
+
+describe("Post /api/sweets", () => {
+    it("should return sweet status 201", async () => {
+        const res = await request(app).post("/api/sweets").send({
+            name: "kaju katli",
+            category: "Nut-Based",
+            price: 50,
+            quantity: 20,
+            
+        })
+    expect(res.statusCode).toBe(201);
+    expect(res.body).toHaveProperty("name", "kaju katli");
+    });
+
+})
