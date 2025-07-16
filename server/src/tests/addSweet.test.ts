31a40e78ab9bc2969100b01b991486604003cfc8
sneha0099 <snehaprajapati678@gmail.com>
2025-07-16 11:35:08 +0530

    style: format addSweet.test.ts using Prettier

--- server/src/tests/addSweet.test.ts
+++ server/src/tests/addSweet.test.ts
@@ -1,28 +1,28 @@
-import request from "supertest";
-import app from "../app";
-import dotenv from "dotenv";    
-import mongoose from "mongoose";
+import request from 'supertest';
+import app from '../app';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import path from 'path';
 
-dotenv.config();
+dotenv.config({ path: path.resolve(__dirname, '../../.env.test') });
 beforeAll(async () => {
-    await mongoose.connect(process.env.MONGO_TEST_URI as string);
+  await mongoose.connect(process.env.MONGO_TEST_URI as string);
 });
 
 afterAll(async () => {
-    await mongoose.connection.close();
+  await mongoose.connection.dropDatabase();
+  await mongoose.connection.close();
 });
 
-describe("Post /api/sweets", () => {
-    it("should return sweet status 201", async () => {
-        const res = await request(app).post("/api/sweets").send({
-            name: "kaju katli",
-            category: "Nut-Based",
-            price: 50,
-            quantity: 20,
-            
-        })
-    expect(res.statusCode).toBe(201);
-    expect(res.body).toHaveProperty("name", "kaju katli");
+describe('Post /api/sweets', () => {
+  it('should return sweet status 201', async () => {
+    const res = await request(app).post('/api/sweets').send({
+      name: 'kaju katli',
+      category: 'Nut-Based',
+      price: 50,
+      quantity: 20,
     });
-
-})
+    expect(res.statusCode).toBe(201);
+    expect(res.body).toHaveProperty('name', 'kaju katli');
+  });
+});
