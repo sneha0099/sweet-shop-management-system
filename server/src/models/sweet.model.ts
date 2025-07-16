0cb7652a03d1e4ae80d5ac37123defc42f7c3023
sneha0099 <snehaprajapati678@gmail.com>
2025-07-16 11:46:21 +0530

    feat: extended categoty options in sweet model (chocolate, candy, pastry)

--- server/src/models/sweet.model.ts
+++ server/src/models/sweet.model.ts
@@ -5,7 +5,7 @@ const sweetSchema = new mongoose.Schema({
   category: { 
     type: String, 
     required: true,
-    enum: ['Nut-Based', 'Milk-Based', 'Vegetable-Based'], 
+    enum: ['Nut-Based', 'Milk-Based', 'Vegetable-Based','Chocolate', 'Candy', 'pastry'], 
   },
   price: { type: Number, required: true },
   quantity: { type: Number, required: true },
