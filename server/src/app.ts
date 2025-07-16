c22e38dd237be73b5eafae9dc43b68204f91b635
sneha0099 <snehaprajapati678@gmail.com>
2025-07-16 10:21:27 +0530

    chore: setup middleware and route registration in app

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,9 +1,25 @@
 import express from 'express';
+import helmet from 'helmet';
+import sweetRoutes from './routes/sweet.route';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import { corsOptions } from './utils/constants';
+
 const app = express();
+
+app.use(cors(corsOptions));
+
+app.use(helmet());
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
 
 app.get('/', (_req, res) => {
   res.send('Sweet Shop API is running');
 });
+
+app.use('/api/sweets', sweetRoutes);
 
 export default app;
