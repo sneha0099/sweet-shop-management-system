ee0e8b0091cc7ec30d86fbc8e7e81bca61933d82
sneha0099 <snehaprajapati678@gmail.com>
2025-07-17 09:41:20 +0530

    chore: add FRONTEND_URL config in utils/constants

--- server/src/utils/constants.ts
+++ server/src/utils/constants.ts
@@ -1,8 +1,8 @@
 export const corsOptions = {
-    origin: process.env.FRONTEND_ORIGIN,
-    methods: ['GET', 'PUT', 'PATCH', 'POST', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true,
-    optionsSuccessStatus: 204, // For legacy browsers like IE11
-    maxAge: 86400, // Cache preflight response for 1 day (in seconds)
+  origin: process.env.FRONTEND_ORIGIN || 'http://localhost:5173',
+  methods: ['GET', 'PUT', 'PATCH', 'POST', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+  optionsSuccessStatus: 204, // For legacy browsers like IE11
+  maxAge: 86400, // Cache preflight response for 1 day (in seconds)
 };
